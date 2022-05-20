1de95bd296357616997c35b60f1b0aa0c09fdaa4
Sattwik <hellosattwik@gmail.com>
2022-05-20 22:01:07 +0530

    refactored popup

--- popup.js
+++ popup.js
@@ -1,11 +1,26 @@
+function handleClick(event) {
+  event.target.parentNode.remove();
+}
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.title && request.playlistLink) {
+    const div = document.createElement('div');
+    div.setAttribute('class', 'playlist-item');
+
     const newElement = document.createElement('a');
+    newElement.setAttribute('class', 'playlist-title');
     newElement.text = request.title;
     newElement.href = request.playlistLink;
-    newElement.setAttribute('id', 'playlist');
 
-    document.querySelector('body').appendChild(newElement);
+    const deleteButton = document.createElement('img');
+    deleteButton.setAttribute('class', 'delete-icon');
+    deleteButton.setAttribute('src', 'images/delete.svg');
+    deleteButton.setAttribute('alt', 'Delete Icon');
+    deleteButton.addEventListener('click', handleClick);
+    div.appendChild(newElement);
+    div.appendChild(deleteButton);
+
+    document.querySelector('body').appendChild(div);
     sendResponse({ status: 'done' });
   }
 });
